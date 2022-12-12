f67de27c16ed289080f89d30738cb2194ba6fc26
sheready <csstlw@gmail.com>
2022-12-12 23:20:26 +0300

    add the Note model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # show only the 1st 50 characters
+        return self.body[0:50]
