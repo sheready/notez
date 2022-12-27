a044dcbdb5303d39f8921e4d82e6c2f78c189244
sheready <csstlw@gmail.com>
2022-12-27 15:21:46 +0300

    debug delete route

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Note
 from .serializers import NoteSerializer
-from notes.api import serializers
+
 # Create your views here.
 @api_view(['GET'])
 def getRoutes(request):
