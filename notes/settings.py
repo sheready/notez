ad23d2cfd94a346857acd8b71d6e8717b1ac55b5
sheready <csstlw@gmail.com>
2023-01-07 15:30:17 +0300

    edit templates

--- notes/settings.py
+++ notes/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/frontend/build/'
+            BASE_DIR / 'frontend/build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
