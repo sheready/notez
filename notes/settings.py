65564ac4fcf0e459524772705106f5ffc5413787
sheready <csstlw@gmail.com>
2023-01-07 09:57:46 +0300

    add secret key for ease in deployment

--- notes/settings.py
+++ notes/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$9z=9=)g5#xj66ggfc!98nx+e3$$x-*%1b*l$$f!t=l2^=teuf'
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
