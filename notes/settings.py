d3c28541dc1943f93ee365efbfb314a91580f283
sheready <csstlw@gmail.com>
2023-01-07 14:41:00 +0300

    edit react template path

--- notes/settings.py
+++ notes/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/build'
+            BASE_DIR / 'frontend/frontend/build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 #link frontend static files
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
+    BASE_DIR / 'frontend/frontend/build/static'
 ]
 
 # Default primary key field type
